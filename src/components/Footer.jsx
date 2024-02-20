b5a64139a1234ab3ab127c7942ae04219145636b
Zack Hanni <zack.hannI@gmail.com>
2024-02-20 00:40:55 -0500

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='bg-black'>
+        <div>
+            <h2 className='text-white text-3xl text-center py-24 '>
+                Footer Section
+            </h2>
+        </div>
+    </footer>
+  )
+}
