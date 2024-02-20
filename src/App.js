7bebc1ca90df033db623f2aeb37e694607025302
Zack Hanni <zack.hannI@gmail.com>
2024-02-20 00:41:16 -0500

    add basic components to homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
+import Hero from "./components/Hero";
+import ImpairmentCards from "./components/ImpairmentCards";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Nav />
-        <h1>hello world</h1>
+        <main className="max-w-screen-lg mx-auto w-full">
+          <Hero />
+          <ImpairmentCards />
+        </main>
+        <Footer />
       </header>
     </div>
   );
