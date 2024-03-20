ef7cab1c39e754852076a5727296a42555c78816
Zack Hanni <zack.hannI@gmail.com>
2024-03-20 16:41:10 -0400

    Create NotFoundPage.jsx

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFoundPage() {
+  return (
+    <>
+      <h1>How'd you get here?</h1>
+      <p>It looks like this page doesn't exist</p>
+    </>
+  );
+}
