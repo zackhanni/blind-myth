f6443eeb76fdbc1d94b3ab35daf1c574474ecd06
Zack Hanni <zack.hannI@gmail.com>
2024-03-20 16:42:55 -0400

    updated from grid to flex positioning

--- src/components/ImpairmentCards.jsx
+++ src/components/ImpairmentCards.jsx
@@ -9,15 +9,15 @@ export default function ImpairmentCards() {
   ];
 
   return (
-    <section className="bg-gray-200 py-16">
-      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 place-items-center space-y-4">
+    <section id="experience" className="bg-gray-200 py-16 px-8">
+      <div className="flex flex-wrap space-x-4 space-y-4 items-center justify-center">
         {impairmentData.map((impairment) => {
           return (
             <div
               key={impairment.name}
               className="bg-gray-500 h-[300px] w-[90%] sm:w-[200px] rounded-lg shadow-sm hover:shadow-2xl hover:-translate-y-2 duration-500"
             >
-              <p className="text-center">{impairment.name}</p>
+              <p className="text-left text-4xl">{impairment.name}</p>
               {/* <img
               src="/pedestrian-crossing.svg"
               alt="blind pedestrian crosswalk"
