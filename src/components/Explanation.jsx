d6068877726b561f0a849e40dd46954e783435ff
Zack Hanni <zack.hannI@gmail.com>
2024-03-20 16:41:40 -0400

    Create Explanation.jsx

---
+++ src/components/Explanation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Explanation() {
+  return (
+    <section className='py-16 px-8 bg-gray-500 text-white'>
+        <div className='space-y-4'>
+            <h2 className='text-3xl text-center'>What is this website?</h2>
+            <p className=''>This is a place to learn and test digital representations of common visual impairments. We work directly with blind individuals to better understand their experiences.</p>
+            <p>Our software engineers take this information and attempt to digitally recreate some of their experiences. We are constantly trying to grow and develope better and more accurate representations. Please bear with us as we continue to develope this web application. </p>
+        </div>
+    </section>
+  )
+}
